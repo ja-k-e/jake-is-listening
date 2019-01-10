d408858b9ef0ed04e8f699abe0e15530469d1642
Jake Albaugh <jake.albaugh@gmail.com>
2019-01-10 12:50:44 -0600

    UPDATE

---
+++ docs/components/EntryComponent.js
@@ -0,0 +1,56 @@
+Vue.component("entry-component", {
+  props: ["entry"],
+  data() {
+    return {
+      iframeLoaded: false
+    };
+  },
+  methods: {
+    cleanOrphan(desc) {
+      return desc.replace(/ ([^ ]+)$/g, "&nbsp;$1");
+    },
+    time(time) {
+      const date = new Date(time);
+      const arr = date
+        .toLocaleDateString()
+        .split("/")
+        .map(s => s.padStart(2, "0"));
+      const yr = [arr[2][2], arr[2][3]].join("");
+      return [yr, arr[0], arr[1]].join(" ");
+    },
+    uriToEmbedUrl(uri) {
+      let [origin, type, id] = uri.split(":");
+      if (origin === "spotify")
+        return `https://open.spotify.com/embed/${type}/${id}`;
+      if (origin === "youtube") return `https://www.youtube.com/embed/${type}`;
+    }
+  },
+  template: entryTemplate()
+});
+
+function entryTemplate() {
+  return /* html */ `
+<article class="entry" :key="entry.embed_uri" :class="{ loaded: iframeLoaded }">
+  <div class="embed">
+    <iframe :src="uriToEmbedUrl(entry.embed_uri)" :key="'iframe-' + entry.embed_uri"
+      @load="iframeLoaded = true"
+      width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"
+    ></iframe>
+  </div>
+
+  <div class="meta">
+    <h3 class="track" v-html="cleanOrphan(entry.track)"></h3>
+    <p class="album" v-html="cleanOrphan(entry.album)"></p>
+    <p class="artist" v-html="cleanOrphan(entry.artist)"></p>
+    <p class="description" v-html="cleanOrphan(entry.description)"></p>
+
+    <links-component :links="entry.links"></links-component>
+    <ul class="links" v-if="entry.extra_links">
+      <li v-for="[title, url] in entry.extra_links">
+        <a :href="url" target="_blank" class="text">{{ title }}</a>
+      </li>
+    </ul>
+  </div>
+</article>
+`;
+}
