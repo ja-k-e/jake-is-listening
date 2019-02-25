e624ac8456671a70f9b59dad9defd9d6e3383380
Jake Albaugh <jake.albaugh@gmail.com>
2019-02-25 15:08:49 -0600

    UPDATE

--- docs/components/EntryComponent.js
+++ docs/components/EntryComponent.js
@@ -23,6 +23,8 @@ Vue.component("entry-component", {
       if (origin === "spotify")
         return `https://open.spotify.com/embed/${type}/${id}`;
       if (origin === "youtube") return `https://www.youtube.com/embed/${type}`;
+      if (origin === "soundcloud")
+        return `https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${type}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`;
     }
   },
   template: entryTemplate()
